124fefe854e86c5c818cf94c7d9ed0850860d80c
Trisha <2200785@slu.edu.ph>
2023-03-30 14:04:08 +0800

    hehe

--- src/IntramsTryout.java
+++ src/IntramsTryout.java
@@ -7,6 +7,7 @@ public class IntramsTryout {
         menu();
         getChoice();
     }
+
     public static void menu(){
         System.out.println("\t\t\t\tWELCOME TO SAMCIS INTRAMS TRYOUT!\n");
         System.out.println("Login as:\n" +
@@ -19,7 +20,8 @@ public class IntramsTryout {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter number: ");
         int l = keyboard.nextInt();
-        switch (l){
+        login(l);
+        /*switch (l){
             case 1:
                 Organizer organizer = new Organizer();
                 organizer.organizerMenu();
@@ -32,7 +34,16 @@ public class IntramsTryout {
             case 3:
                 //For student
                 break;
-        }
+        }*/
     }
 
+    public static void login(int choice) {
+        Scanner scanner = new Scanner(System.in);
+        switch (choice) {
+            case 1 -> System.out.print("\nLogin as Organizer\nEnter ID Number: ");
+            case 2 -> System.out.print("\nLogin as Coach\nEnter Employee ID Number: ");
+            case 3 -> System.out.print("\nLogin as Student\nEnter Student ID Number: ");
+        }
+        String idLogin = scanner.nextLine();
+    }
 }
